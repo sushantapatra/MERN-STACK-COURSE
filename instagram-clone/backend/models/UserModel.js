14706862c33119834edeb4fedaeca5cc2db8ad4a
Sushanta <development.experimen@gmail.com>
2024-08-15 09:24:12 +0530

    add user model

---
+++ instagram-clone/backend/models/UserModel.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+	{
+		username: {
+			type: String,
+			required: [true, "Username is required"],
+			unique: true,
+			min: [4, "Must be at least 4, got {VALUE}"],
+			max: [12, "Must be at least 12, got {VALUE}"],
+		},
+		email: {
+			type: String,
+			required: [true, "Email is required"],
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: [true, "Password is required"],
+			min: [4, "Must be at least 4, got {VALUE}"],
+			max: [12, "Must be at least 12, got {VALUE}"],
+		},
+		profilePicture: { type: String, default: "" },
+		bio: { type: String, default: "" },
+		gender: { type: String, enum: ["male", "female"] },
+		followers: [{ type: mongoose.Schema.ObjectId, ref: "User" }],
+		following: [{ type: mongoose.Schema.ObjectId, ref: "User" }],
+		posts: [{ type: mongoose.Schema.ObjectId, ref: "Post" }],
+		bookmarks: [{ type: mongoose.Schema.ObjectId, ref: "Post" }],
+	},
+	{ timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
